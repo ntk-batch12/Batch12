189be4f56424c85b0c2696faeabf33041af0648e
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-10 21:53:20 -0400

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -6,4 +6,11 @@ public class GitHomework {
 	public void display() {
 		System.out.println("John Cena");
 	}
+	/**
+			This is coming from GitHub
+   			Java is fun
+      			Git is EASY
+	 		API is not fun..
+
+	*/
 }

0f88d388080322d56f5d2472679d58629c343c2c
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-10 22:06:07 -0400

    removed all codee

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,16 +1,5 @@
 package homework;
 
 public class GitHomework {
-
-
-	public void display() {
-		System.out.println("John Cena");
-	}
-	/**
-			This is coming from GitHub
-   			Java is fun
-      			Git is EASY
-	 		API is not fun..
-
-	*/
+ 
 }

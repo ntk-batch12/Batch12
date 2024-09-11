8c909bb609ad696aef499341f7de639d357ef7ac
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-10 21:35:13 -0400

    test

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,5 +2,12 @@ package homework;
 
 public class GitHomework {
 
+	public void getCity() {
+		System.out.println("Reston");
+	}
+	
+	public void getCountry() {
+		System.out.println("USA");
+	}
 	
 }

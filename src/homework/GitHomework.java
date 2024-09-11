1d5d2a2f30550648a6c0b2ef43fea03763140d39
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-10 21:15:12 -0400

    removed code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,14 +2,5 @@ package homework;
 
 public class GitHomework {
 
-	public static void main(String[] args) {
-		System.out.println("BATCH 12: Git Class Lecture");
-		int age = 12;
-		String name = "John";
-		// This is coming from github
-	}
 	
-	public static void add(int a, int b) {
-		System.out.println(a + b);
-	}
 }
